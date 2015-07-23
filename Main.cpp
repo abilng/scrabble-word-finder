08e231385b058d606e79c09801224d53b8b90ba6
ABIL N GEORGE <mail@abilng.in>
2015-07-23 14:04:26 +0530

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : Scrabble.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "ScrabbleDict.h"
+
+
+
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	/*
+	 if(argc < 3) {
+	 cerr << "USAGE " << argv[0] << " DICTFILE CHARINHAND" << endl;
+	 return -1;
+	 }
+	 */
+	string inputFile = "sowpods.txt";
+	string charsInHand = "ABCD"; //argv[2];
+
+	ScrabbleDict scrabbledict(inputFile);
+	string validWord = scrabbledict.getMaxScoreWord(charsInHand);
+	cout << validWord <<endl;
+
+	return 0;
+}
